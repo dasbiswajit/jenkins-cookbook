90bfd59f192e751b2b023dcf56f323f6c0a6ffff
Biswajit Das <biswajitbangalore@gmail.com>
2017-05-09 19:22:48 +0530

    Changed cookbook version

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'biswajitbangalore@gmail.com'
 license 'All rights reserved'
 description 'This cookbook is a chef compliant cookbook'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '1.0.0'
+version '1.0.1'
 %w(redhat suse).each do |os|
   supports os
 end
