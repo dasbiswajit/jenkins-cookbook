aabb81a8ecd787bfd937abdf79bca9449620f579
Biswajit Das <biswajitbangalore@gmail.com>
2017-05-09 15:24:24 +0100

    updated cookbook

--- recipes/default.rb
+++ recipes/default.rb
@@ -5,14 +5,14 @@
 Log.debug("Node attributes from" + node.default['test-cookbook']['attribute1'])
 
 # test file resource
-file '/tmp/testfile.log' do
+file '/tmp/test.log' do
   content 'This is a test file'
   mode '0755'
   owner 'root'
   group 'root' 
 end
 # test file resource
-file '/tmp/testfile1.log' do
+file '/tmp/test1.log' do
   content 'This is a test file'
   mode '0755'
   owner 'root'
