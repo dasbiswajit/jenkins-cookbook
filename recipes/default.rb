015b2996f08be304d3ede02370ddf77364811ce1
Biswajit Das <biswajitbangalore@gmail.com>
2018-03-21 18:05:30 +0000

    Update default.rb

--- recipes/default.rb
+++ recipes/default.rb
@@ -4,18 +4,16 @@
 # Get the current version from metadata
 Log.debug("Node attributes from" + node.default['test-cookbook']['attribute1'])
 
-# test file resource
-file '/tmp/testavinash.log' do
-  content 'This is a test file'
-  mode '0755'
-  owner 'root'
-  group 'root' 
-end
-# test file resource
-file '/tmp/testbiswajit.log' do
-  content 'This is a test file'
-  mode '0755'
-  owner 'root'
-  group 'root' 
-end
+bash 'jenkins Installation' do
+  code <<-EOH
+            sudo wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat/jenkins.repo
+            sleep 10
+            sudo rpm --import https://jenkins-ci.org/redhat/jenkins-ci.org.key
+            sleep 10
+            sudo yum install jenkins -y
+            sleep 30
+            sudo service jenkins start
 
+
+   EOH
+end
