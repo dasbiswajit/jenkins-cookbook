9845c663c637554c05fbb5de66597bf135e6ab6c
Biswajit Das <biswajitbangalore@gmail.com>
2018-03-22 18:30:45 +0000

    Update default.rb

--- recipes/default.rb
+++ recipes/default.rb
@@ -7,14 +7,25 @@ Log.debug("Node attributes from" + node.default['test-cookbook']['attribute1'])
 bash 'jenkins Installation' do
   cwd ::File.dirname('/var/lib/jenkins')
   code <<-EOH
-            sudo wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat/jenkins.repo
-            sleep 10
-            sudo rpm --import https://jenkins-ci.org/redhat/jenkins-ci.org.key
-            sleep 10
-            sudo yum install jenkins -y
-            sleep 30
-            sudo service jenkins start
-
-
+    logfile=/home/ec2-user/logfile.txt
+    echo "Info:: Downloading Jenkins repo" >> $logfile
+    sudo wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat/jenkins.repo >> $logfile
+    sleep 10
+    echo "Info:: Downloading Jenkins key" >> $logfile
+    sudo rpm --import https://jenkins-ci.org/redhat/jenkins-ci.org.key >> $logfile
+    sleep 10
+    echo "Info:: Installing Jenkins" >> $logfile
+    sudo yum install jenkins -y >> $logfile
+    sleep 30
+    echo "download jenkins plugins script" >> $logfile
+    mkdir /tmp/jenkins_scripts >> $logfile     
+    cd /tmp/jenkins_scripts >> $logfile     
+    git clone https://github.com/dasbiswajit/jenkins-script.git >> $logfile     
+    cd jenkins-script/ >> $logfile     
+    sudo sh Jenkins_plugin.sh role-strategy >> $logfile   
+    sleep 30            
+    sudo service jenkins restart
+    sleep 20
+    echo "Info:: Jenkins installed successfully" >> $logfile
    EOH
 end
