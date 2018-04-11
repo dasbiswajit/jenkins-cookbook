0460ebedc23b52f783f40ca6183f9e1b81e17fce
Biswajit Das <biswajitbangalore@gmail.com>
2018-04-11 09:29:38 +0000

    updated Jenkins user name

--- attributes/default.rb
+++ attributes/default.rb
@@ -3,8 +3,11 @@
 
 # default attributes for basic splunk install configuration
 node.default['test-cookbook']['attribute1'] = 'value from Attribute'
-node.default['test-cookbook']['hashval'] = 'tthhkjhasdhlkasdhlkasd'
+
+# Jenkins master attributes
 node.default['test-cookbook']['jenkins-master']['port'] = '8081'
+node.default['test-cookbook']['jenkins-master']['user_name']= 'admin'
+
 
 
 
