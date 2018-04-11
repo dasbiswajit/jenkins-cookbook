a6ee3d2eef82875946b44178c9a6f826536c9d10
Biswajit Das <biswajitbangalore@gmail.com>
2018-04-11 09:35:01 +0000

    updated slave attribute

--- attributes/default.rb
+++ attributes/default.rb
@@ -6,7 +6,10 @@ node.default['test-cookbook']['attribute1'] = 'value from Attribute'
 
 # Jenkins master attributes
 node.default['test-cookbook']['jenkins-master']['port'] = '8081'
-node.default['test-cookbook']['jenkins-master']['user_name']= 'admin'
+node.default['test-cookbook']['jenkins-master']['user-name']= 'admin'
+
+# Jenkins slave related attributes
+node.default['test-cookbook']['jenkins-slave']['slave-val'] = 'value'
 
 
 
