a297b90c5fed58b9bf331640ebd63a8f35323937
Biswajit Das <biswajitbangalore@gmail.com>
2018-04-11 09:25:31 +0000

    Added jenkins master port

--- attributes/default.rb
+++ attributes/default.rb
@@ -4,3 +4,7 @@
 # default attributes for basic splunk install configuration
 node.default['test-cookbook']['attribute1'] = 'value from Attribute'
 node.default['test-cookbook']['hashval'] = 'tthhkjhasdhlkasdhlkasd'
+node.default['test-cookbook']['jenkins-master']['port'] = '8081'
+
+
+
