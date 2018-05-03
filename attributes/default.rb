d1de70c9c150b9337790c791c0fdf70ba890f316
Biswajit Das <biswajitbangalore@gmail.com>
2018-05-03 16:53:09 +0530

    updated efsid

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,5 +2,5 @@
 
 node.default['jenkins-cookbook']['jenkins-master']['profile'] = 'security'
 node.default['jenkins-cookbook']['jenkins-master']['region'] = 'eu-west-1'
-node.default['jenkins-cookbook']['jenkins-master']['efs_id'] = 'fs-80990b49'
+node.default['jenkins-cookbook']['jenkins-master']['efs_id'] = 'efsid'
 node.default['jenkins-cookbook']['jenkins-master']['efs_suffix'] = 'efs.eu-west-1.amazonaws.com'
